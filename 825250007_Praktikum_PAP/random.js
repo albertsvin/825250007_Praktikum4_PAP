7e717109a7795434ff09c7ae9d0e04b983e9d4f6
albertsvin <albertdavinson260@gmail.com>
2026-09-12 14:03:17 +0700

    Update random.js

--- 825250007_Praktikum_PAP/random.js
+++ 825250007_Praktikum_PAP/random.js
@@ -2,13 +2,10 @@ const container = document.getElementById('container-id');
     function buttonClick() {
     container.innerHTML = "<div><p>Nama : Alberta Davin</p><p>NIM : 825250007</p></div>";
 }
-        
-    document.getElementById('tombol').addEventListener('click', buttonClick);
-        
+    document.getElementById('tombol').addEventListener('click', buttonClick);        
     document.getElementById('tombol2').addEventListener('click', function() {
         container.innerHTML = "";
     });
-        // Event handling dengan arrow function
     document.addEventListener('keydown', (event) => {
         console.log('Tombol keyboard ditekan:', event.key);
     });
